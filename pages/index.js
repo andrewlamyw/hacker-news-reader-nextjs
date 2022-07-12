a4e543a7bcfe81f941856cd970b1cef667615fa5
andrewlamyw <andrewlam.yw@gmail.com>
2022-07-13 01:33:05 +0800

    Call Hacker News API

--- pages/index.js
+++ pages/index.js
@@ -1,52 +1,39 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import useSWR from 'swr';
+import chunk from 'lodash/chunk';
+import { useState } from 'react';
+
+const fetcher = (...args) => fetch(...args).then((res) => res.json());
+
+const Item = ({ id }) => {
+  const { data } = useSWR(
+    `https://hacker-news.firebaseio.com/v0/item/${id}.json`,
+    fetcher
+  );
+
+  return <li>{data?.title}</li>;
+};
 
 export default function Home() {
+  const { data: topStoriesId } = useSWR(
+    'https://hacker-news.firebaseio.com/v0/topstories.json',
+    fetcher
+  );
+  const [page, setPage] = useState(1);
+  const topStoriesIdChunck = chunk(topStoriesId, 30);
+
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
+        <title>Hacker News Reader - Next.js</title>
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        {topStoriesIdChunck &&
+          topStoriesIdChunck[page - 1]?.map((id) => (
+            <Item id={id} key={id}></Item>
+          ))}
       </main>
 
       <footer className={styles.footer}>
