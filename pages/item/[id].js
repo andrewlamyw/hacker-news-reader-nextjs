2b640d90b4960c735a3a59a8b45709c176b48b53
Andrew Lam <andrewlam.yw@gmail.com>
2022-07-13 16:29:24 +0800

    Add document title and favicon

--- pages/item/[id].js
+++ pages/item/[id].js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useRouter } from "next/router";
 import { useItem } from "../../hooks.js";
+import Head from "next/head";
 
 const Item = () => {
   const router = useRouter();
@@ -10,13 +11,19 @@ const Item = () => {
   if (error) return "Something went wrong";
 
   return (
-    <div className="container mx-auto px-4">
-      <h1 className="text-3xl font-extrabold text-gray-800 py-10">
-        {data?.title}
-      </h1>
+    <>
+      <Head>
+        <title>{data?.title}</title>
+      </Head>
 
-      <main dangerouslySetInnerHTML={{ __html: data?.text }}></main>
-    </div>
+      <div className="container mx-auto px-4">
+        <h1 className="text-3xl font-extrabold text-gray-800 py-10">
+          {data?.title}
+        </h1>
+
+        <main dangerouslySetInnerHTML={{ __html: data?.text }}></main>
+      </div>
+    </>
   );
 };
 
